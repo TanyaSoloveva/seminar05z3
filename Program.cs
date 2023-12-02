42ad4d3b1f0d93d2e52fa6d45a1529e0660bd11a
Tatyana Soloveva <tanuysya@mail.ru>
2023-12-02 14:27:35 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 3: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с 
+// наименьшей суммой элементов
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int row, int col, int leftRange, int rightRange)
+{
+    int[,] tempMatrix = new int[row, col];
+    Random rand = new Random();
+
+    for (int i = 0; i < tempMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempMatrix.GetLength(1); j++)
+        {
+            tempMatrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return tempMatrix;
+}
+
+void PrintMatrix(int[,] matrixForPrint)
+{
+    for (int i = 0; i < matrixForPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixForPrint.GetLength(1); j++)
+        {
+            System.Console.Write(matrixForPrint[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+// -----------------------------
+
+int rows = ReadInt("Введите количество строк в матрице: ");
+
+int cols = ReadInt("Введите количество столбцов в матрице: ");
+
+int[,] matrix = GenerateMatrix(rows, cols, 0, 10);
+
+PrintMatrix(matrix);
+Console.WriteLine();
+int minSumLine = 0;
+int sumLine = SumLineElements(matrix, 0);
+for (int i = 1; i < matrix.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(matrix, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой элементов ");
+
+
+int SumLineElements(int[,] matrix, int i)
+{
+  int sumLine = matrix[i,0];
+  for (int j = 1; j < matrix.GetLength(1); j++)
+  {
+    sumLine += matrix[i,j];
+  }
+  return sumLine;
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
